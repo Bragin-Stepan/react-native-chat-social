2915d879a36f9351b7302e52d67a57f684442d22
Bragin Stepan <braginstepan@gmail.com>
2024-09-22 10:49:53 +0500

    fix

--- utils/time-format.ts
+++ utils/time-format.ts
@@ -1,12 +1,23 @@
 import moment from 'moment';
 import 'moment/locale/ru';
+
+// Обновляем сокращение для месяцев
+moment.updateLocale('ru', {
+  monthsShort: {
+    format: 'янв фев мар апр май июн июл авг сен окт ноя дек'.split(' '),
+    standalone: 'янв фев мар апр май июн июл авг сен окт ноя дек'.split(' '),
+  },
+});
+
 moment.locale('ru');
 
 export const formatLastMessageTime = (dateString: string): string => {
   const date = moment(dateString);
   const today = moment().startOf('day');
   const yesterday = moment().subtract(1, 'day').startOf('day');
+  const weekAgo = moment().subtract(7, 'days').startOf('day');
 
+  // Для теста
   if (dateString === '2027-09-20T14:30:00Z') {
     return date.format('HH:mm');
   }
@@ -14,7 +25,9 @@ export const formatLastMessageTime = (dateString: string): string => {
   if (date.isSame(today, 'day')) {
     return date.format('HH:mm'); // "16:00"
   } else if (date.isSame(yesterday, 'day')) {
-    return 'Вчера'; // Вчера
+    return 'Вчера'; // "Вчера"
+  } else if (date.isAfter(weekAgo)) {
+    return date.format('dd'); // "Пятница"
   } else if (date.isSame(today, 'year')) {
     return date.format('D MMM'); // "17 сен"
   } else {
